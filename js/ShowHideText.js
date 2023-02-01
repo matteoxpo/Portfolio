d0f9844a993bbdb480ac02565397bde6ecf21a05
Xpomin <s-hromin@mail.ru>
2023-02-01 18:37:00 +0700

    add

--- js/ShowHideText.js
+++ js/ShowHideText.js
@@ -1,3 +1,12 @@
 function ShowHideText(from) {
     $("." + from).toggle('show');
 }
+
+$(document).ready(function () {
+    var words = $('.word').text().split(' ');
+    var html = '';
+    $.each(words, function () {
+        html += '<span style="font-size:120%;color:red;text-transform:uppercase;">' + this.substring(0, 1) + '</span>' + this.substring(1) + ' ';
+    });
+    $('.word').html(html);
+});
